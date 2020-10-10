5a8142a6e7475e333978175007ad01be9136a21e
IvanUstin <ivan.ustin@bk.ru>
2020-10-10 22:34:24 +0300

    second Task

---
+++ src/secondTask/Task2_1.java
@@ -0,0 +1,32 @@
+package secondTask;
+
+import java.util.Scanner;
+
+/* Даны два угла треугольника (в градусах). Определить, существует ли
+такой треугольник, и если да, то будет ли он прямоугольным.
+ */
+public class Task2_1 {
+    public static void main(String[] args) {
+        System.out.print("Enter two angles by degrees:\n first: ");
+
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+
+        while (a >= 180 || a <= 0) {
+            System.out.print("We need a flat triangle, let's do it again.\n" +
+                    "first: ");
+            a = scanner.nextDouble();
+        }
+
+        System.out.print("second: ");
+        double b = scanner.nextDouble();
+
+        if (b >= 180 || b <= 0) System.out.println("Life teaches nothing.");
+        else if (a + b < 180) {
+            System.out.println("There is a triangle with " +
+                    "same angles.");
+            if (a == 90 || b == 90 || a + b == 90) System.out.println("It's " +
+                    "rectangular.");
+        } else System.out.println("Such triangle doesn't exist.");
+    }
+}
---
+++ src/secondTask/Task2_3.java
@@ -0,0 +1,37 @@
+package secondTask;
+
+import java.util.Scanner;
+
+/*Даны три точки А(х1,у1), В(х2,у2) и С(х3,у3).
+ Определить, будут ли они расположены на одной прямой.
+ */
+public class Task2_3 {
+    public static void main(String[] args) {
+        System.out.print("Enter x1: ");
+        Scanner scanner = new Scanner(System.in);
+        double x1 = scanner.nextDouble();
+
+        System.out.print("Enter y1: ");
+        double y1 = scanner.nextDouble();
+
+
+        System.out.print("Enter x2: ");
+        double x2 = scanner.nextDouble();
+
+
+        System.out.print("Enter y2: ");
+        double y2 = scanner.nextDouble();
+
+
+        System.out.print("Enter x3: ");
+        double x3 = scanner.nextDouble();
+
+
+        System.out.print("Enter y3: ");
+        double y3 = scanner.nextDouble();
+
+        if ((x3 - x1)/(x2 - x1) == (y3 - y1)/(y2 - y1))
+            System.out.println("Dots lie on the same line.");
+        else System.out.println("Dots don't lie on the same line.");
+    }
+}
